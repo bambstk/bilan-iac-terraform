2b47f5b98d5c0ee067894901f6c3983dcda3b57b
Leith <112956932+bambstk@users.noreply.github.com>
2026-08-12 10:55:26 +0200

    Fix: postgre pwd update

--- terraform/modules/postgresql/main.tf
+++ terraform/modules/postgresql/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_postgresql_flexible_server" "postgres" {
   sku_name   = "B_Standard_B1ms"
 
     lifecycle {
-    ignore_changes = [zone, administrator_password]
+    ignore_changes = [zone]
   }
 
   tags = var.tags
